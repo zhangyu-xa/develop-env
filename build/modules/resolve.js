2758f264f91a98ff454d7aeb17dc9fccef65526e
zhangyu <zhangyu-xa@netposa.com>
2018-04-24 13:34:39 +0800

    [add]添加resolve选项

--- build/modules/resolve.js
+++ build/modules/resolve.js
@@ -4,9 +4,15 @@
 const Utils = require("../utils");
 
 module.exports = {
+	//定义别名
 	alias: {
-		//别名
 		component: Utils.resolve("src/component")
 	},
-	extensions: [".js", ".vue", ".json"]
+	//后缀名自动补全
+	extensions: [".js", ".vue", ".json", ".css"],
+	//依赖查找位置定义
+	modules: [
+		Utils.resolve("node_modules"),
+		Utils.resolve("src")
+	]
 }
