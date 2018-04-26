694456ff0d17794ce4b6339cfa096fcc2b4d0f81
zhangyu <zhangyu-xa@netposa.com>
2018-04-26 19:37:57 +0800

    [add]添加espromise测试

--- src/index.js
+++ src/index.js
@@ -5,3 +5,19 @@ import "./index.css";
 import ss from "component/tool";
 
 console.log("this is a demo");
+
+function mySyncFun() {
+	var p = new Promise(function (resolve, reject) {
+		setTimeout(function () {
+			console.log("执行完成");
+			resolve("this is 数据");
+		}, 2000);
+	});
+	return p;
+}
+
+setTimeout(function () {
+	mySyncFun().then(function (data) {
+		console.log(data);
+	});
+}, 100);
