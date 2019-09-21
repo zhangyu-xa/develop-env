33f760a49cfc509a2b5a818767548a544e635323
zhangyu <zy_graduate@sina.com>
2019-09-21 16:56:12 +0800

    init

--- src/index.js
+++ src/index.js
@@ -1,38 +1,12 @@
-/**
- * Created by Zhangyu on 2018/4/19.
- */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-if (module.hot) {
-	module.hot.accept();
-}
-import "./css/index.css";
-import ss from "component/tool";
+ReactDOM.render(<App />, document.getElementById('root'));
 
-function log(...args){
-
-}
-
-console.log("this is a demo", process.env.NODE_ENV);
-
-function mySyncFun() {
-	var p = new Promise(function (resolve, reject) {
-		setTimeout(function () {
-			resolve("this is 数据，当前版本是:" + version);
-		}, 2000);
-	});
-	return p;
-}
-
-setTimeout(function () {
-	mySyncFun().then(function (data) {
-		console.log(data);
-	});
-}, 100);
-
-var divObj = document.getElementsByClassName("demo");
-
-//divObj[0].innerText = "demo";
-
-window.onload = function () {
-	console.log(" onload is finished.");
-}
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
